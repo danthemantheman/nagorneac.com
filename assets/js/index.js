29a13089a5372783f3c1e52755da102f8128fe84
Daniel Nagorneac <dannagorneac@gmail.com>
2018-06-10 21:08:12 -0500

    added modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,23 @@
 $(function() {
-  $('.thumbnail').on('click', function() {
+  $('.thumbnail').on('click', function(event) {
+    const $thumbnailElement = $(event.currentTarget);
+    const fullsizeImageUrl = $thumbnailElement.data('fullsize');
 
-    $('#modal').fadeIn();
+  $('.fullsize').attr('src',fullsizeImageUrl);
 
+  $('body').css('overflow', 'hidden');
+
+    $('.modal').fadeIn();
   });
 
-
+  $('.close').on('click', closeModal);
+  $('.modal').on('click', closeModal);
 });
 
+function closeModal() {
+  $('.modal').fadeOut();
+
+  $('body').css('overflow', 'scroll');
+
+
+}
